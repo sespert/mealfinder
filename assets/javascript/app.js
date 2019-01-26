edac1f082f5a3e3d31279fbe2e8ae87fbc9c1e5c
sespertg <sespert@gmail.com>
2019-01-26 10:55:24 -0500

    Adding things to my branch

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,33 @@
-
+$(document).ready(function(){
 // Initiate Firebase
-var config = {
-    apiKey: "AIzaSyCy-Ar-cQpeBMIXNgJvrohQ-FzvlmfTabQ",
-    authDomain: "mealfinder-bad48.firebaseapp.com",
-    databaseURL: "https://mealfinder-bad48.firebaseio.com",
-    projectId: "mealfinder-bad48",
-    storageBucket: "mealfinder-bad48.appspot.com",
-    messagingSenderId: "739056825436"
-};
-firebase.initializeApp(config);
 
-// Call Spoonacular API
+    var config = {
+        apiKey: "AIzaSyCy-Ar-cQpeBMIXNgJvrohQ-FzvlmfTabQ",
+        authDomain: "mealfinder-bad48.firebaseapp.com",
+        databaseURL: "https://mealfinder-bad48.firebaseio.com",
+        projectId: "mealfinder-bad48",
+        storageBucket: "mealfinder-bad48.appspot.com",
+        messagingSenderId: "739056825436"
+    };
+    firebase.initializeApp(config);
+
+    //$("#user-input").val().trim();
+    
+    function validateUserInput() {
+        var name = $("#user-input").val().trim();
+        if (name == "") {
+          $("#alert").text("Name must be filled out");
+          return false;
+        } else {
+            console.log(name);
+        }
+      }
+    
+      $("#submit").on("click", function(event){
+        return validateUserInput();
+
+      });
+      // Call Spoonacular API
 
 var apiKey = "5xw4QaTb3Wmsh8AQrQQBKOLf93yXp10FyXNjsnN6kLNdE5w3P6"
 var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ingredients=apples%2Cflour%2Csugar";
@@ -24,4 +41,9 @@ $.ajax({
 }).then(function (response) {
     console.log(response)
 })
+
+});
+
+
+
 
